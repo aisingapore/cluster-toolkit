3053e3c7cffc0e17302f55fb29f61487eeac84e5
wheynelau <waynelau15045@gmail.com>
2024-08-23 15:26:10 +0800

    add sssd

---
+++ examples/machine-learning/a3-highgpu-8g/v5-legacy/scripts/sssd-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# both client and server needs to be installed
+set -e 
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt install sssd-ldap ldap-utils -y
+
+cat > /etc/sssd/sssd.conf <<EOF
+[sssd]
+config_file_version = 2
+domains = example.com
+
+[domain/example.com]
+id_provider = ldap
+auth_provider = ldap
+ldap_uri = ldap://${LDAP_SERVER_IP}
+cache_credentials = True
+ldap_search_base = dc=example,dc=com
+ldap_tls_reqcert = never
+EOF
+
+chmod 0600 /etc/sssd/sssd.conf
+
+# verify if sss in nsswitch.conf
+
+sed -i 's/compat/compat sss/g' /etc/nsswitch.conf
+
+systemctl enable sssd
+systemctl restart sssd
+pam-auth-update --enable mkhomedir
